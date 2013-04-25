0d60d64022f1ac2dc8f7bd68b1ff63da01dab4db
alexander fedorov <elf607@ya.ru>
2013-04-25 11:42:16 +0600

    making progress

--- vpn_center_gui.py
+++ vpn_center_gui.py
@@ -16,6 +16,9 @@ class MainForm(QtGui.QMainWindow):
         # super(Window, self).__init__(parent)
         self.ui.setupUi(self)
 
+        self.server_list_layout = QtGui.QVBoxLayout()
+        self.ui.server_list_container.setLayout(self.server_list_layout)
+
         self.connect(self.ui.add_server_button, QtCore.SIGNAL('pressed()'),self.add_server_to_list)
 
     def add_server_to_list(self):
@@ -33,20 +36,56 @@ class MainForm(QtGui.QMainWindow):
         ip_row_layout = QtGui.QHBoxLayout()
         
         ip_row_label = QtGui.QLabel("IP Address")
+        ip_row_delimeter = QtGui.QLabel(" : ")
         ip_row_value = QtGui.QLineEdit()
 
         ip_row_layout.addWidget(ip_row_label)
+        ip_row_layout.addWidget(ip_row_delimeter)
         ip_row_layout.addWidget(ip_row_value)
 
+        ##### user configs #####
+
+        user_row_layout = QtGui.QHBoxLayout()
+        
+        user_row_label = QtGui.QLabel("User Name")
+        user_row_delimeter = QtGui.QLabel(" : ")
+        user_row_value = QtGui.QLineEdit()
+
+        user_row_layout.addWidget(user_row_label)
+        user_row_layout.addWidget(user_row_delimeter)
+        user_row_layout.addWidget(user_row_value)
+
+        ##### passwd configs #####
+
+        passwd_row_layout = QtGui.QHBoxLayout()
+        
+        passwd_row_label = QtGui.QLabel("Passwd")
+        passwd_row_delimeter = QtGui.QLabel(" : ")
+        passwd_row_value = QtGui.QLineEdit()
+
+        passwd_row_layout.addWidget(passwd_row_label)
+        passwd_row_layout.addWidget(passwd_row_delimeter)
+        passwd_row_layout.addWidget(passwd_row_value)
+
+        ##### server layout #####
 
         server_config_layout = QtGui.QVBoxLayout()
 
         server_config_layout.addLayout(ip_row_layout)
+        server_config_layout.addLayout(user_row_layout)
+        server_config_layout.addLayout(passwd_row_layout)
 
 
-        group_box.setLayout(server_config_layout)            
+        group_box.setLayout(server_config_layout)  
+        group_box.adjustSize()          
+
         
-        self.ui.server_list_layout.addWidget(group_box)
+
+        self.server_list_layout.addWidget(group_box)
+        
+        
+
+        # self.ui.server_list_layout.addWidget(group_box)
 
 
 if __name__ == "__main__":
